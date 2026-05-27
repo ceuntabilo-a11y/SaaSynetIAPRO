487cc3940d319ae94016aec24ac9ceb0d3e9a9a6
Dorian Muñoz <omcuriosoty18@gmail.com>
2026-05-27 10:12:17 -0400

    fix: import attachments with .js extension for ESM compatibility

--- api/send-email.ts
+++ api/send-email.ts
@@ -1,5 +1,5 @@
 import { Resend } from "resend";
-import { DICOM_ATTACHMENTS } from "./attachments";
+import { DICOM_ATTACHMENTS } from "./attachments.js";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
